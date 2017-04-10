f1c1349dbe59b985cc4a70a993d3313c6946bb37
HASEENA BANU BINTE GHULAM MOHAMED _ <haseenagm.2014@smu.edu.sg>
2017-04-10 19:07:34 +0800

    helpingHands.R edited

--- GeospatialProject/helpingHands.R
+++ GeospatialProject/helpingHands.R
@@ -1,8 +1,11 @@
 library(leaflet)
 library(rgdal)
+library(raster)
 #mpsz_svy21 <- readOGR(dsn="shapefile", layer="MP14_SUBZONE_NO_SEA_PL")
 #shops <- read.csv ("attributetable/shops.csv")
-#Footpath<- readOGR(dsn="shapefile", layer="Footpath")
+#footpath<- readOGR(dsn="shapefile", layer="footpaths")
+#footpath<-spTransform(footpath, CRS("+proj=utm +zone=48N +datum=WGS84"))
+#footpath<-spTransform(footpath, CRS("+proj=longlat +datum=WGS84"))
 #NeighbourhoodAmenities <- read.csv ("attributetable/NeighbourhoodAmenities.csv")
 #HDB <- read.csv ("attributetable/HDB.csv")
 
@@ -23,10 +26,13 @@ library(rgdal)
 server<-(function(input, output) {
 
   
-  
   output$map <- renderLeaflet({
     df <- shops[shops$CATEGORY == input$CATEGORY,]  
     m<-leaflet()  %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
       setView(103.777395,1.428903, zoom = 17)
       m%>%addTiles() %>%
       addCircles(~LONGITUDE,~LATITUDE,
@@ -41,8 +47,8 @@ server<-(function(input, output) {
         addMarkers(~LONGITUDE,~LATITUDE,
                    popup=~POI_NAME,data=NeighbourhoodAmenities
                    
-        )
-   #addPolylines(data=Footpath, color="red")   
+        )%>%
+   addPolylines(data=footpath, color="red")   
       
   
 
@@ -57,19 +63,19 @@ library(leaflet)
 
 ui<-fluidPage(
   navbarPage(title="Helping Hands",
-             tabPanel(title = "Map View",
-     fluidRow(   
-              column(8, (leafletOutput("map","100%",600))),
-              column(4,fluidPage(tags$h4("-Displaying ramps 500m buffered"),
-                                 tags$h4(""),
-                                 tags$h4("Sensitivity Analysis"),sliderInput(inputId = "num",
-                                   label = "Choose a Distance",
-                                   value = 500, min = 500, max = 600))
-              )
-      )
+             tabPanel(title = "Map View"
               ),
   navbarMenu(title = "Accessibility",
-             tabPanel(title = "Hansen Potential Model"),
+             tabPanel(title = "Hansen Potential Model"
+                      ,
+                      fluidRow(   
+                        column(8, (leafletOutput("map","100%",600))),
+                        column(4,fluidPage(tags$h4("Accessibility Analysis"),
+                                           tags$h4("")
+                        )
+                        )
+                      )
+                      ),
                   
              
              tabPanel(title = "Weighted Matrix"),
@@ -80,7 +86,7 @@ ui<-fluidPage(
                       
                         
               
-             tabPanel(title="Statistics",
+             tabPanel(title="Statistics"),
              fluidRow(column(8,""), 
                       column(4,fluidPage(tags$h4("HISTOGRAM: Distribution of public amenities according to number and type of public amenities."),
 tags$br(),
@@ -92,7 +98,6 @@ tags$h6( " <Fig2>Public Amenities[X],  Postal Code [Y] ")
 )
            )
   )
-)
             
  
              
